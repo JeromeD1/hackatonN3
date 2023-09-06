f18451f9b79bfc4124ffe4955dec5fa8d3e47d93
Jerome Desouches <jerome.desouches@gmail.com>
2023-09-06 11:26:40 +0200

    preparation du context et des routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,40 @@
+import { Routes, Route } from "react-router-dom"
+import { useState, useMemo, useEffect } from "react"
+import MyContext from "./components/MyContext"
+
 import Home from "./pages/Home"
 
 import "./App.css"
 
 function App() {
+
+  const [user, setUser] = useState({}) // A REMPLACER PAR LE STATE VOULU
+
+// stockage de l'état initial de user, setUser et users via un useMemo
+const valeursFourniesDansMyContextProvider = useMemo(
+  () => ({
+    user, // A REMPLACER OU AJOUTER EN FONCTION DU BESOIN
+    setUser,
+  }),
+  [
+    user,
+    setUser,
+  ]
+)
+
+useEffect(() => {
+  console.info("Mettre ici les données à récupérer via un axios")
+})
+
   return (
     <div className="App">
-      <Home />
+
+<MyContext.Provider value={valeursFourniesDansMyContextProvider}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </MyContext.Provider>
+      
       <p>coucou</p>
     </div>
   )
