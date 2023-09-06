4a0c9b2688a722956f34cdcda6ab8ae32c962bdc
SebastienANTICOURT <sebastien.anticourt@live.fr>
2023-09-06 17:51:49 +0200

    NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,17 @@
 import { Routes, Route } from "react-router-dom"
-import { useState, useMemo, useEffect } from "react"
-import MyContext from "./components/MyContext"
-
+import NavBar from "./components/NavBar"
 import Home from "./pages/Home"
 
 import "./App.css"
 
 function App() {
-
-  const [user, setUser] = useState({}) // A REMPLACER PAR LE STATE VOULU
-
-// stockage de l'état initial de user, setUser et users via un useMemo
-const valeursFourniesDansMyContextProvider = useMemo(
-  () => ({
-    user, // A REMPLACER OU AJOUTER EN FONCTION DU BESOIN
-    setUser,
-  }),
-  [
-    user,
-    setUser,
-  ]
-)
-
-useEffect(() => {
-  console.info("Mettre ici les données à récupérer via un axios")
-})
-
   return (
     <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
 
-<MyContext.Provider value={valeursFourniesDansMyContextProvider}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </MyContext.Provider>
-      
       <p>coucou</p>
     </div>
   )
