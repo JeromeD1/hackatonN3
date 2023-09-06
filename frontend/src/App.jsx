fe3efb2ae8b1a175992c4de9527a5f45934ccba8
Jerome Desouches <jerome.desouches@gmail.com>
2023-09-06 15:06:19 +0200

    installation react router dom et ajout BrowserRouter dans main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,34 +7,29 @@ import Home from "./pages/Home"
 import "./App.css"
 
 function App() {
-
   const [user, setUser] = useState({}) // A REMPLACER PAR LE STATE VOULU
 
-// stockage de l'état initial de user, setUser et users via un useMemo
-const valeursFourniesDansMyContextProvider = useMemo(
-  () => ({
-    user, // A REMPLACER OU AJOUTER EN FONCTION DU BESOIN
-    setUser,
-  }),
-  [
-    user,
-    setUser,
-  ]
-)
+  // stockage de l'état initial de user, setUser et users via un useMemo
+  const valeursFourniesDansMyContextProvider = useMemo(
+    () => ({
+      user, // A REMPLACER OU AJOUTER EN FONCTION DU BESOIN
+      setUser,
+    }),
+    [user, setUser]
+  )
 
-useEffect(() => {
-  console.info("Mettre ici les données à récupérer via un axios")
-})
+  useEffect(() => {
+    console.info("Mettre ici les données à récupérer via un axios")
+  })
 
   return (
     <div className="App">
-
-<MyContext.Provider value={valeursFourniesDansMyContextProvider}>
+      <MyContext.Provider value={valeursFourniesDansMyContextProvider}>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </MyContext.Provider>
-      
+
       <p>coucou</p>
     </div>
   )
