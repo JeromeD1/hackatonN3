7837a244c68e46226753b04b9044c77d603a3d13
DcDpnT <dupont.d33720@gmail.com>
2023-09-07 15:49:38 +0200

    search bar with context

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -2,33 +2,32 @@ import "./Search.scss"
 import React, { useState } from "react"
 import { useSearchContext } from "./SearchContext"
 
-
 const Search = () => {
-    const { setSearch } = useSearchContext();
-    const [ inputValue, setInputValue] = useState("");
+  const { setSearch } = useSearchContext()
+  const [inputValue, setInputValue] = useState("")
 
-    const handleInputChange = (e) => {
-        setInputValue(e.target.value);
-    };
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
+  }
 
-    const handleSendSearch = () => {
-        setSearch(inputValue);
-        setInputValue("");
-    };
-    
-    return (
+  const handleSendSearch = () => {
+    setSearch(inputValue)
+    setInputValue("")
+  }
+
+  return (
     <div className="search">
-        <input 
-            type="text" 
-            name="searchBar" 
-            id="searchBar"
-            placeholder="Ask to the AI..."
-            value={inputValue}
-            onChange={handleInputChange} 
-        />  
-        <button onClick={handleSendSearch}>Send</button>
+      <input
+        type="text"
+        name="searchBar"
+        id="searchBar"
+        placeholder="Ask to the AI..."
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleSendSearch}>Send</button>
     </div>
-    )
+  )
 }
 
-export default Search;
+export default Search
