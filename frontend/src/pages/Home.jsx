b285c522ccdcfa8cb419d99908e7175a6b2c4770
Jerome Desouches <jerome.desouches@gmail.com>
2023-09-07 06:48:37 +0200

    amélioration du filtre, quand rien n'est sélectionné, tout s'affiche

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+/* global L */
+
 import { useState, useEffect } from "react"
 // import axios from "axios"
 import "./Home.scss"
@@ -9,8 +11,6 @@ import volcan from "../assets/images/volcan.png"
 import incendie from "../assets/images/incendie.png"
 import tsunami from "../assets/images/tsunami.png"
 import radioactivite from "../assets/images/radioactivite.png"
-
-let L // A METTRE EN COMMENTAIRE SINON CA NE FONCTIONNE PAS
 
 const iconTornade = L.icon({
   iconUrl: tornade,
@@ -43,48 +43,55 @@ const iconRadioactivite = L.icon({
 })
 
 const events = [
-  {
-    type: "tornade",
-    icone: iconTornade,
-    lat: 48.8566,
-    lng: 2.35,
-  },
-  {
-    type: "meteorite",
-    icone: iconMeteorite,
-    lat: 49,
-    lng: 2,
-  },
-  {
-    type: "volcan",
-    icone: iconVolcan,
-    lat: 48.5,
-    lng: 1.5,
-  },
-  {
-    type: "incendie",
-    icone: iconIncendie,
-    lat: 49.1,
-    lng: 2.45,
-  },
-  {
-    type: "tsunami",
-    icone: iconTsunami,
-    lat: 48.5,
-    lng: 2.5,
-  },
+  { type: "tornade", icone: iconTornade, lat: 48.8566, lng: 2.35 },
+  { type: "meteorite", icone: iconMeteorite, lat: 49.0, lng: 2.0 },
+  { type: "volcan", icone: iconVolcan, lat: 48.5, lng: 1.5 },
+  { type: "incendie", icone: iconIncendie, lat: 49.1, lng: 2.45 },
+  { type: "tsunami", icone: iconTsunami, lat: 48.5, lng: 2.5 },
+  { type: "radioactivite", icone: iconRadioactivite, lat: 48.75, lng: 1.9 },
+  { type: "tornade", icone: iconTornade, lat: 43.2965, lng: 5.3698 },
+  { type: "meteorite", icone: iconMeteorite, lat: 45.764, lng: 4.8357 },
+  { type: "volcan", icone: iconVolcan, lat: 44.8378, lng: -0.5792 },
+  { type: "incendie", icone: iconIncendie, lat: 43.6047, lng: 1.4442 },
+  { type: "tsunami", icone: iconTsunami, lat: 47.2184, lng: -1.5536 },
   {
     type: "radioactivite",
     icone: iconRadioactivite,
-    lat: 48.75,
-    lng: 1.9,
+    lat: 48.1173,
+    lng: -1.6778,
   },
+  { type: "tornade", icone: iconTornade, lat: 49.4944, lng: 0.1079 },
+  { type: "meteorite", icone: iconMeteorite, lat: 50.9513, lng: 1.8587 },
+  { type: "tornade", icone: iconTornade, lat: 45.1885, lng: 5.7245 },
+  { type: "meteorite", icone: iconMeteorite, lat: 47.3216, lng: 5.0415 },
+  { type: "volcan", icone: iconVolcan, lat: 43.6108, lng: 3.8767 },
+  { type: "incendie", icone: iconIncendie, lat: 43.1242, lng: 6.1286 },
+  { type: "tsunami", icone: iconTsunami, lat: 43.7009, lng: 7.2684 },
   {
     type: "radioactivite",
     icone: iconRadioactivite,
-    lat: 49,
-    lng: 1.9,
+    lat: 49.2583,
+    lng: 4.0317,
   },
+  { type: "tornade", icone: iconTornade, lat: 47.7508, lng: 7.3359 },
+  { type: "meteorite", icone: iconMeteorite, lat: 48.6921, lng: 6.1844 },
+  { type: "volcan", icone: iconVolcan, lat: 49.1193, lng: 6.1757 },
+  { type: "incendie", icone: iconIncendie, lat: 48.5839, lng: 7.7455 },
+  { type: "tornade", icone: iconTornade, lat: 48.3904, lng: -4.4861 },
+  { type: "meteorite", icone: iconMeteorite, lat: 47.9029, lng: 1.909 },
+  { type: "volcan", icone: iconVolcan, lat: 48.1173, lng: -1.6778 },
+  { type: "incendie", icone: iconIncendie, lat: 49.4944, lng: 0.1079 },
+  { type: "tsunami", icone: iconTsunami, lat: 50.9513, lng: 1.8587 },
+  {
+    type: "radioactivite",
+    icone: iconRadioactivite,
+    lat: 43.8374,
+    lng: -2.2192,
+  },
+  { type: "tornade", icone: iconTornade, lat: 43.2965, lng: 5.3698 },
+  { type: "meteorite", icone: iconMeteorite, lat: 43.1242, lng: 6.1286 },
+  { type: "volcan", icone: iconVolcan, lat: 43.7009, lng: 7.2684 },
+  { type: "incendie", icone: iconIncendie, lat: 43.6108, lng: 3.8767 },
 ]
 
 export default function Home() {
@@ -139,22 +146,33 @@ export default function Home() {
   }, [mounted])
 
   useEffect(() => {
-    const eventsSelected = filtersEvent
-      .map((filter) => (filter.selected ? filter.type : null))
-      .filter((item) => item !== null)
+    if (mounted) {
+      const eventsSelected = filtersEvent
+        .map((filter) => (filter.selected ? filter.type : null))
+        .filter((item) => item !== null)
 
-    // Supprimer tous les marqueurs existants de la carte
-    markers.forEach((marker) => marker.remove())
+      // Suppression de tous les marqueurs existants sur la carte
+      markers.forEach((marker) => marker.remove())
 
-    // Créer les nouveaux marqueurs et les ajouter à la carte
-    const newMarkers = events
-      .filter((item) => eventsSelected.includes(item.type))
-      .map((item) =>
-        L.marker([item.lat, item.lng], { icon: item.icone }).addTo(map)
-      )
-
-    // Mettre à jour l'état des marqueurs
-    setMarkers(newMarkers)
+      // Création des nouveaux marqueurs et les ajout à la carte
+      if (eventsSelected.length === 0) {
+        const newMarkers = events.map((item) =>
+          L.marker([item.lat, item.lng], {
+            icon: item.icone,
+          }).addTo(map)
+        )
+        // Mise à jour de l'état des marqueurs
+        setMarkers(newMarkers)
+      } else {
+        const newMarkers = events
+          .filter((item) => eventsSelected.includes(item.type))
+          .map((item) =>
+            L.marker([item.lat, item.lng], { icon: item.icone }).addTo(map)
+          )
+        // Mise à jour de l'état des marqueurs
+        setMarkers(newMarkers)
+      }
+    }
   }, [filtersEvent])
 
   return (
