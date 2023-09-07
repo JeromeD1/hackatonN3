70aac2d65b77e6f49baafcbc6c63e3c15ce65a14
SebastienANTICOURT <sebastien.anticourt@live.fr>
2023-09-07 15:57:18 +0200

    title dans la navBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import { useState, useMemo, useEffect } from "react"
 import MyContext from "./components/MyContext"
-import NavBar from "./components/NavBar"
+import NavBar from "./components/navBar"
 import Home from "./pages/Home"
 
 import "./App.css"
--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -7,23 +7,48 @@ import teteEclaire from "../assets/teteEclaire.png"
 import cloche from "../assets/cloche.png"
 
 function NavBar() {
+  const speak = (text) => {
+    var msg = new SpeechSynthesisUtterance(text)
+    window.speechSynthesis.speak(msg)
+  }
+
+  const imageAlt = document
+    .querySelector(".astrolabe-icon img")
+    .getAttribute("alt")
+  speak(imageAlt)
   return (
     <div className="navBar">
       <ul>
         <li className="astrolabe-icon">
-          <img src={astrolabe} alt="astrolabe" />
+          <img src={astrolabe} alt="logo" title="logo" />
         </li>
         <li className="surevie-text">
           <p>Sûrevie</p>
         </li>
         <li className="icon-group">
-          <img src={oeil} alt="personnes malevoyantes" />
-          <img src={chaiseRoulante} alt="personnes à mobilité réduite" />
-          <img src={oreille} alt="personnes malentendantes" />
-          <img src={teteEclaire} alt="teteEclaire" />
+          <img
+            src={oeil}
+            alt="personnes malevoyantes"
+            title="personnes malevoyantes"
+          />
+          <img
+            src={chaiseRoulante}
+            alt="personnes à mobilité réduite"
+            title="personnes à mobilité réduite"
+          />
+          <img
+            src={oreille}
+            alt="personnes malentendantes"
+            title="personnes malentendantes"
+          />
+          <img
+            src={teteEclaire}
+            alt="maladie mentale"
+            title="maladie mentale"
+          />
         </li>
         <li className="cloche-icon">
-          <img src={cloche} alt="alertes" />
+          <img src={cloche} alt="alertes" title="alertes" />
         </li>
       </ul>
     </div>
