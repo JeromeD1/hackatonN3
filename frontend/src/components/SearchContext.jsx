7f0ea289ff60a1afc595557948146b42b5564668
Shark-44 <joanny.bernardeau@gmail.com>
2023-09-07 16:26:03 +0200

    recup code damien

---
+++ frontend/src/components/SearchContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useContext, useState } from "react"
+
+const SearchContext = createContext()
+
+export const useSearchContext = () => {
+  return useContext(SearchContext)
+}
+
+export const SearchProvider = ({ children }) => {
+  const [search, setSearch] = useState("")
+
+  return (
+    <SearchContext.Provider value={{ search, setSearch }}>
+      {children}
+    </SearchContext.Provider>
+  )
+}
