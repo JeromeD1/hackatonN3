3db026046cb25abcaf9953bdd64aadc0a9be23ba
Vivien Floureck <vivienfloureck@outlook.com>
2023-09-07 14:18:28 +0200

    popup ok

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,15 @@
+/* global L */
 import { useState, useEffect } from "react"
 // import axios from "axios"
 import "./Home.scss"
 import { cities } from "../assets/variables/cities"
+import MonFormulaire from "../components/MonFormulaire"
 
 export default function Home() {
   const [mounted, setMounted] = useState(false)
   const [map, setMap] = useState(null)
   const [citySelected, setCitySelected] = useState(cities[20])
-  let L
+  // let L
 
   const handleChangeCity = (e) => {
     const newCitySelected = cities.filter(
@@ -37,15 +39,58 @@ export default function Home() {
     }
   }, [mounted])
 
-  return (
-    <main className="main-home">
-      <div id="map"></div>
+  const [showModal, setShowModal] = useState(true)
 
-      <select value={citySelected.city} onChange={handleChangeCity}>
-        {cities.map((city) => (
-          <option key={city.city}>{city.city}</option>
-        ))}
-      </select>
-    </main>
+  useEffect(() => {
+    setShowModal(true)
+  }, [])
+
+  const closeModal = () => {
+    setShowModal(false)
+  }
+  // const openModal = () => {
+  //   setShowModal(true)
+  // }
+  return (
+    <>
+      <div>
+        {showModal && (
+          <div className="modal-wrapper">
+            <div className="modal">
+              <div className="modal-content">
+                <img
+                  className="porte"
+                  src="../src/assets/images/portebunker.png"
+                  alt="porte d'un bunker"
+                />
+                <h2>Sûrevie</h2>
+                <button className="findAbri" onClick={closeModal}>
+                  Trouver votre abri
+                </button>
+                <div className="alerte">
+                  <img
+                    src="../src/assets/images/alerte.png"
+                    alt="cloche d'alerte"
+                  />
+                  <p>Rejoignez le réseau d'alerte pour ne ratez aucune infos</p>
+                </div>
+                <div className="register">
+                  <MonFormulaire />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <main className="main-home">
+        <div id="map"></div>
+
+        <select value={citySelected.city} onChange={handleChangeCity}>
+          {cities.map((city) => (
+            <option key={city.city}>{city.city}</option>
+          ))}
+        </select>
+      </main>
+    </>
   )
 }
