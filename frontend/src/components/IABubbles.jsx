005218753469bfbea081f09ff6dc7653f52ce044
SebastienANTICOURT <sebastien.anticourt@live.fr>
2023-09-07 18:08:40 +0200

    IA qui reconnait les events

--- frontend/src/components/IABubbles.jsx
+++ frontend/src/components/IABubbles.jsx
@@ -1,17 +1,88 @@
-// import { useState } from "react"
+import { useState } from "react"
 import bulleIA from "../assets/images/bulleIA.png"
 import ReponseIA from "../assets/images/ReponseIA.png"
 import "./IABubbles.scss"
 
 function IABubble() {
+  const [inputValue, setInputValue] = useState("")
+  const [message, setMessage] = useState("message")
+
+  const events = [
+    { type: "tornade" },
+    { type: "meteorite" },
+    { type: "volcan" },
+    { type: "incendie" },
+    { type: "tsunami" },
+    { type: "radioactivite" },
+    { type: "tornade" },
+    { type: "meteorite" },
+    { type: "volcan" },
+    { type: "incendie" },
+    { type: "tsunami" },
+    { type: "radioactivite" },
+    { type: "tornade" },
+    { type: "meteorite" },
+    { type: "tornade" },
+    { type: "meteorite" },
+    { type: "volcan" },
+    { type: "incendie" },
+    { type: "tsunami" },
+    { type: "radioactivite" },
+    { type: "tornade" },
+    { type: "meteorite" },
+    { type: "volcan" },
+    { type: "incendie" },
+    { type: "tornade" },
+    { type: "meteorite" },
+    { type: "volcan" },
+    { type: "incendie" },
+    { type: "tsunami" },
+    { type: "radioactivite" },
+    { type: "tornade" },
+    { type: "meteorite" },
+    { type: "volcan" },
+    { type: "incendie" },
+  ]
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value.toLowerCase())
+  }
+  const handleSendSearch = () => {
+    const loweredValue = inputValue.toLowerCase()
+
+    // Find first matching event type within the user's input.
+    const foundEvent = events.find((event) => loweredValue.includes(event.type))
+
+    if (foundEvent) {
+      // Count how many of the found event type exist in the array.
+      const count = events.filter(
+        (event) => event.type === foundEvent.type
+      ).length
+      setMessage(`Attention! Il y a ${count} ${foundEvent.type} en approche!`)
+    } else {
+      setMessage("message")
+    }
+    setInputValue("")
+  }
+
   return (
     <div className="reponseIA">
       <div style={{ display: postMessage.photoVisible ? "none" : "flex" }}>
-        <img className="bulle" src={bulleIA} alt="hi" />
-        <h4>message</h4>
+        <img className="bulle" src={bulleIA} alt="Bulle IA" />
+        <h4>{message}</h4>
       </div>
-      <img className="IA" src={ReponseIA} alt="hi" />
+      <img className="IA" src={ReponseIA} alt="Réponse IA" />
+      <input
+        type="text"
+        name="searchBar"
+        id="searchBar"
+        placeholder="Ask to the AI..."
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleSendSearch}>Send</button>
     </div>
   )
 }
+
 export default IABubble
---
+++ frontend/src/components/Search.jsx
@@ -0,0 +1,7 @@
+// import "./Seach.scss"
+
+const Search = () => {
+  return <div className="search"></div>
+}
+
+export default Search
