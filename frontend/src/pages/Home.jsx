2d3c2fca92d7429a074249a16f1ae55f4fa05726
Jerome Desouches <jerome.desouches@gmail.com>
2023-09-07 09:32:54 +0200

    debut ajout info evenement

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,94 +5,14 @@ import { useState, useEffect } from "react"
 import "./Home.scss"
 import { cities } from "../assets/variables/cities"
 import { filters } from "../assets/variables/filters"
-import tornade from "../assets/images/tornade.png"
-import meteorite from "../assets/images/meteorite.png"
-import volcan from "../assets/images/volcan.png"
-import incendie from "../assets/images/incendie.png"
-import tsunami from "../assets/images/tsunami.png"
-import radioactivite from "../assets/images/radioactivite.png"
+import { events } from "../assets/variables/events"
 
-const iconTornade = L.icon({
-  iconUrl: tornade,
-  iconSize: [80, 120],
-})
-
-const iconMeteorite = L.icon({
-  iconUrl: meteorite,
-  iconSize: [120, 120],
-})
-
-const iconVolcan = L.icon({
-  iconUrl: volcan,
-  iconSize: [80, 120],
-})
-
-const iconIncendie = L.icon({
-  iconUrl: incendie,
-  iconSize: [80, 100],
-})
-
-const iconTsunami = L.icon({
-  iconUrl: tsunami,
-  iconSize: [100, 100],
-})
-
-const iconRadioactivite = L.icon({
-  iconUrl: radioactivite,
-  iconSize: [80, 80],
-})
-
-const events = [
-  { type: "tornade", icone: iconTornade, lat: 48.8566, lng: 2.35 },
-  { type: "meteorite", icone: iconMeteorite, lat: 49.0, lng: 2.0 },
-  { type: "volcan", icone: iconVolcan, lat: 48.5, lng: 1.5 },
-  { type: "incendie", icone: iconIncendie, lat: 49.1, lng: 2.45 },
-  { type: "tsunami", icone: iconTsunami, lat: 48.5, lng: 2.5 },
-  { type: "radioactivite", icone: iconRadioactivite, lat: 48.75, lng: 1.9 },
-  { type: "tornade", icone: iconTornade, lat: 43.2965, lng: 5.3698 },
-  { type: "meteorite", icone: iconMeteorite, lat: 45.764, lng: 4.8357 },
-  { type: "volcan", icone: iconVolcan, lat: 44.8378, lng: -0.5792 },
-  { type: "incendie", icone: iconIncendie, lat: 43.6047, lng: 1.4442 },
-  { type: "tsunami", icone: iconTsunami, lat: 47.2184, lng: -1.5536 },
-  {
-    type: "radioactivite",
-    icone: iconRadioactivite,
-    lat: 48.1173,
-    lng: -1.6778,
-  },
-  { type: "tornade", icone: iconTornade, lat: 49.4944, lng: 0.1079 },
-  { type: "meteorite", icone: iconMeteorite, lat: 50.9513, lng: 1.8587 },
-  { type: "tornade", icone: iconTornade, lat: 45.1885, lng: 5.7245 },
-  { type: "meteorite", icone: iconMeteorite, lat: 47.3216, lng: 5.0415 },
-  { type: "volcan", icone: iconVolcan, lat: 43.6108, lng: 3.8767 },
-  { type: "incendie", icone: iconIncendie, lat: 43.1242, lng: 6.1286 },
-  { type: "tsunami", icone: iconTsunami, lat: 43.7009, lng: 7.2684 },
-  {
-    type: "radioactivite",
-    icone: iconRadioactivite,
-    lat: 49.2583,
-    lng: 4.0317,
-  },
-  { type: "tornade", icone: iconTornade, lat: 47.7508, lng: 7.3359 },
-  { type: "meteorite", icone: iconMeteorite, lat: 48.6921, lng: 6.1844 },
-  { type: "volcan", icone: iconVolcan, lat: 49.1193, lng: 6.1757 },
-  { type: "incendie", icone: iconIncendie, lat: 48.5839, lng: 7.7455 },
-  { type: "tornade", icone: iconTornade, lat: 48.3904, lng: -4.4861 },
-  { type: "meteorite", icone: iconMeteorite, lat: 47.9029, lng: 1.909 },
-  { type: "volcan", icone: iconVolcan, lat: 48.1173, lng: -1.6778 },
-  { type: "incendie", icone: iconIncendie, lat: 49.4944, lng: 0.1079 },
-  { type: "tsunami", icone: iconTsunami, lat: 50.9513, lng: 1.8587 },
-  {
-    type: "radioactivite",
-    icone: iconRadioactivite,
-    lat: 43.8374,
-    lng: -2.2192,
-  },
-  { type: "tornade", icone: iconTornade, lat: 43.2965, lng: 5.3698 },
-  { type: "meteorite", icone: iconMeteorite, lat: 43.1242, lng: 6.1286 },
-  { type: "volcan", icone: iconVolcan, lat: 43.7009, lng: 7.2684 },
-  { type: "incendie", icone: iconIncendie, lat: 43.6108, lng: 3.8767 },
-]
+// import tornade from "../assets/images/tornade.png"
+// import meteorite from "../assets/images/meteorite.png"
+// import volcan from "../assets/images/volcan.png"
+// import incendie from "../assets/images/incendie.png"
+// import tsunami from "../assets/images/tsunami.png"
+// import radioactivite from "../assets/images/radioactivite.png"
 
 export default function Home() {
   const [mounted, setMounted] = useState(false)
@@ -140,6 +60,16 @@ export default function Home() {
           icon: item.icone,
         }).addTo(mapInstance)
       )
+
+      // const newMarkers = events.map((item) =>
+      //   {
+      //    let mark= L.marker([item.lat, item.lng], {
+      //     icon: item.icone,
+      //   }).addTo(mapInstance)
+
+      //   mark.bindTooltip('Texte à afficher').openTooltip();
+      // }
+      // )
 
       setMarkers(newMarkers)
     }
