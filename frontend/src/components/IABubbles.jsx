497376675feaeb5b926a56280c83946ceb0107d9
SebastienANTICOURT <sebastien.anticourt@live.fr>
2023-09-08 13:37:59 +0200

    ajout IA

--- frontend/src/components/IABubbles.jsx
+++ frontend/src/components/IABubbles.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import bulleIA from "../assets/images/bulleIA.png"
-import ReponseIA from "../assets/images/ReponseIA.png"
+import imageIAPouceBas from "../assets/images/imageIAPouceBas.png"
+import pouceHaut from "../assets/images/pouceHaut.png"
+
 import "./IABubbles.scss"
 
-function IABubble() {
+function IABubble({ onEnterPress }) {
   const [inputValue, setInputValue] = useState("")
-  const [message, setMessage] = useState("message")
+  const [message, setMessage] = useState("posez vos questions")
+  const [displayedMessage, setDisplayedMessage] = useState("")
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [show, setShow] = useState("hidden")
+  const [pouce, setPouce] = useState(pouceHaut)
 
   const events = [
     { type: "tornade" },
@@ -44,40 +50,102 @@ function IABubble() {
     { type: "incendie" },
   ]
 
+  // const changeImage = () => {
+  //   setPouce = true
+  // }
+
   const handleInputChange = (e) => {
     setInputValue(e.target.value.toLowerCase())
   }
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSendSearch()
+
+      if (onEnterPress) {
+        onEnterPress()
+      }
+    }
+  }
+
   const handleSendSearch = () => {
     const loweredValue = inputValue.toLowerCase()
-    const foundEvent = events.find((event) => loweredValue.includes(event.type))
-
-    if (foundEvent) {
-      const count = events.filter(
-        (event) => event.type === foundEvent.type
-      ).length
-      setMessage(`Attention! Il y a ${count} ${foundEvent.type} en approche!`)
+    setShow("visible")
+    setTimeout(() => {
+      setShow("hidden")
+    }, 50000)
+    let newMessage = "posez vos questions"
+    if (loweredValue.includes("abri")) {
+      newMessage =
+        "cliquer sur votre position pour connaitre l'abri le plus proche"
+    } else if (loweredValue.includes("securite")) {
+      newMessage =
+        "cliquer sur le bouton afficher les abris pour voir les abris sur la carte"
+    } else if (loweredValue.includes("sauve")) {
+      newMessage = "Non, vous allez mourir!"
+      setPouce(imageIAPouceBas)
+      setTimeout(() => {
+        setPouce(pouceHaut)
+      }, 5000)
     } else {
-      setMessage("message")
+      const foundEvent = events.find((event) =>
+        loweredValue.includes(event.type)
+      )
+      if (foundEvent) {
+        const count = events.filter(
+          (event) => event.type === foundEvent.type
+        ).length
+        newMessage = `Attention! Il y a ${count} ${foundEvent.type}s en approche!`
+      }
     }
+
+    setMessage(newMessage)
     setInputValue("")
+
+    // This part checks if the `onEnterPress` callback exists
+    // and passes the decided message to it.
+    if (onEnterPress) {
+      onEnterPress(newMessage)
+    }
   }
+
+  useEffect(() => {
+    setDisplayedMessage("")
+    setCurrentIndex(0)
+  }, [message])
+
+  useEffect(() => {
+    if (currentIndex < message.length) {
+      const timer = setTimeout(() => {
+        setDisplayedMessage(
+          (prevMessage) => prevMessage + message[currentIndex]
+        )
+        setCurrentIndex((prevIndex) => prevIndex + 1)
+      }, 100)
+
+      return () => clearTimeout(timer)
+    }
+  }, [currentIndex, message])
 
   return (
     <div className="reponseIA">
-      <div style={{ display: postMessage.photoVisible ? "none" : "flex" }}>
+      {/* <div style={{ display: postMessage.photoVisible ? "none" : "flex" }}> */}
+      <div className="Labulle" style={{ visibility: show }}>
         <img className="bulle" src={bulleIA} alt="Bulle IA" />
-        <h4>{message}</h4>
+        <h4>{displayedMessage}</h4>
       </div>
-      <img className="IA" src={ReponseIA} alt="Réponse IA" />
+      <img className="IA" src={pouce} alt="Réponse IA" />
       <input
+        className="barreRecherche"
         type="text"
         name="searchBar"
         id="searchBar"
         placeholder="Ask to the AI..."
         value={inputValue}
         onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
       />
-      <button onClick={handleSendSearch}>Send</button>
+      {/* <button onClick={handleSendSearch}>Send</button> */}
     </div>
   )
 }
